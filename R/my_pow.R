a4c2e23c132887801aa6c773844adffd6b6dd8c5
Junzhao Liu <zhaoaiws@gmail.com>
2020-03-02 16:55:57 -0800

    Set up package

---
+++ R/my_pow.R
@@ -0,0 +1,17 @@
+#' Power function
+#'
+#' this function raises input to a power
+#' @param x Numeric in inout to be raised to the power of \code{power}
+#' @param power Numeric input for the power that \code{x} will be raised to
+#'    defaults to \code{2}
+#'
+#' @return Numeric representing \code{x} raised to the power of \code{power}
+#'
+#' @examples
+#' my_pow(4)
+#' my_pow(4, power = 3)
+#'
+#' @export
+my_pow <- function(x, power = 2) {
+  return(x^power)
+}
